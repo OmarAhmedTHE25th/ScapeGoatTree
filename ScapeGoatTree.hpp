93707e263524b890f8f014b35aadb1b8b2e0a4ea
ahmedmohamedioskareem-cloud <ahmedmohamedioskareem@gmail.com>
2025-12-15 13:20:47 +0200

    Update ScapeGoatTree.hpp

--- ScapeGoatTree.hpp
+++ ScapeGoatTree.hpp
@@ -25,7 +25,7 @@ public:
     ScapeGoatTree();
     // Inserts a new value into the tree 
     void insert(int value);
-    void ScapeGoatTree::deleteValue(int value)
+    void deleteValue(int value);
     ~ScapeGoatTree(){postorderTraversal(root);}
 
     void isBalanced() const;
