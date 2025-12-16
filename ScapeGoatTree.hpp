d8443ca1ebbed52b6a9246e30b059f4ad26862da
Omar Ahmed Abdel Hameed <omar.ahmed.nes6@gmail.com>
2025-12-16 19:17:07 +0200

    Enhance ScapeGoatTree with display methods
    Added display methods for different traversal orders and modified return types for isBalanced and getDisplayBuffer.

--- ScapeGoatTree.hpp
+++ ScapeGoatTree.hpp
@@ -5,6 +5,9 @@
 #ifndef SCAPEGOATTREE_SCAPEGOATTREE_HPP
 #define SCAPEGOATTREE_SCAPEGOATTREE_HPP
 
+#include <sstream>
+#include <string>
+
 #include "Node.hpp"
 
 class ScapeGoatTree {
@@ -15,23 +18,32 @@ class ScapeGoatTree {
     Node* rebuildTree(int start,int end,Node* parent_node);
     void inorderTraversal(const Node *node, int &i);
     static void postorderTraversal(const Node* node);
-     void preorderTraversal(const Node* node);
+    void preorderTraversal(const Node* node);
+    void displayPreOrder(const Node* node); // for display
+    void displayInOrder(const Node* node); // for display
+    void  displayPostOrder(const Node* node) ; // for display
+    mutable std::string displayBuffer;
     Node* root{};
     int array[100]{};
     int nNodes{};
 
-
 public:
+
     ScapeGoatTree();
     // Inserts a new value into the tree 
     void insert(int value);
     void deleteValue(int value);
     ~ScapeGoatTree(){postorderTraversal(root);}
-    void isBalanced() const;
+
+    std::string isBalanced() const;
+    std::string& getDisplayBuffer() const;
     ScapeGoatTree(const ScapeGoatTree &Otree);
     ScapeGoatTree operator+(const ScapeGoatTree &goat_tree) const;
     ScapeGoatTree& operator=(const ScapeGoatTree& Otree);
     [[nodiscard]] bool search(int key) const;
+    void displayPreOrder(); // for display
+    void displayInOrder() ; // for display
+    void displayPostOrder() ; // for display
     bool operator[](int value) const;
     bool operator==(const ScapeGoatTree &tree) const;
     bool operator!=(const ScapeGoatTree &tree) const;
