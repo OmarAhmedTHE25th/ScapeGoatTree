dd2fc09a02f393fc8765c86f570544eb2fe55020
Omar Ahmed Abdel Hameed <omar.ahmed.nes6@gmail.com>
2025-12-30 13:14:28 +0200

    Refactor Vector class for improved memory management and dynamic resizing

--- CPP/vector.hpp
+++ CPP/vector.hpp
@@ -3,33 +3,29 @@
 
 template <typename T>
 class Vector {
-    T* data;
-    size_t _size;
-    size_t _capacity;
-
-    void reserve(const size_t new_cap) {
-        _capacity = (_capacity == 0) ? 1 : _capacity * 2;
-        T* newData = new T[_capacity];
-        for (int i = 0; i < _size; ++i) newData[i] = data[i];
-        delete[] data;
-        data = newData;
-    }
+    unsigned int _size = 50;
+    int nElements = 0;
+    T* data = new T[_size]{};
 
 public:
-    Vector() : data(nullptr), _size(0), _capacity(0) {}
     ~Vector() { delete[] data; }
-    void push_back(const T& value) {
-        if (_size == _capacity) {
-            reserve(_capacity == 0 ? 1 : _capacity * 2);
-        }
-        data[_size++] = value;
-    }
+
     // Disable copying to prevent crashes
     Vector(const Vector&)=delete;
-    [[nodiscard]] size_t size() const { return _size; }
-
-    T& operator[](size_t index) { return data[index]; }
-    const T& operator[](size_t index) const { return data[index]; }
+    [[nodiscard]] unsigned int size() const { return _size; }
+    void push_back(const T& value) {
+       if (nElements >= _size) {
+           _size *= 2;
+           T* newData = new T[_size]{};
+           for (unsigned int i = 0; i < nElements; ++i)
+               newData[i] = data[i];
+           delete[] data;
+           data = newData;
+       }
+       data[nElements++] = value;
+    }
+    T& operator[](unsigned int index) { return data[index]; }
+    const T& operator[](unsigned int index) const { return data[index]; }
     T operator[](int i) const { return data[i]; }
 };
 #endif
