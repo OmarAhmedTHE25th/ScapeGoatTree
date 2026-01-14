a4545aee178ebe31ce8554e7d8870248691e89b2
Omar Ahmed Abdel Hameed <omar.ahmed.nes6@gmail.com>
2026-01-14 16:53:53 +0200

    Update Doxygen configuration to include .hpp and .tpp file patterns and set extension mapping for .tpp files

---
+++ html/queue_8tpp.js
@@ -0,0 +1,4 @@
+var queue_8tpp =
+[
+    [ "TREE_QUEUE_TPP", "queue_8tpp.html#a199e2b1e32fe3beff142e16e4a53fb7f", null ]
+];
---
+++ html/scapegoat_tree_8tpp.js
@@ -0,0 +1,4 @@
+var scapegoat_tree_8tpp =
+[
+    [ "TREE_SCAPEGOATTREE_TPP", "scapegoat_tree_8tpp.html#a00cce2b959d332ad26e9243f84b9cf66", null ]
+];
