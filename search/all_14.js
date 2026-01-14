bae3a7fe2c6e91279c3155fabe7827623c603296
OmarAhmedTHE25th <OmarAhmedTHE25th@users.noreply.github.com>
2026-01-14 03:37:13 +0000

    deploy: 75e0eb7c74344efdf9ee4d4e5cc5e250b6263371

--- search/all_14.js
+++ search/all_14.js
@@ -4,7 +4,8 @@ var searchData=
   ['undo_1',['undo',['../classScapeGoatTree.html#a693ee93867d1092ca56b98becfddb21d',1,'ScapeGoatTree::undo()'],['../iTree_8hpp.html#a44c68a04cafe3d38456832196c7bf22aa269940bcad6fddf5c68d3e67f767c3d1',1,'UNDO:&#160;iTree.hpp']]],
   ['undostack_2',['undoStack',['../classScapeGoatTree.html#a6cf65741abbbef5a152bb8033b733e3d',1,'ScapeGoatTree']]],
   ['unit_20tests_3',['Run Unit Tests',['../index.html#autotoc_md32',1,'']]],
-  ['usage_4',['Advanced Usage',['../index.html#autotoc_md9',1,'']]],
-  ['usage_20examples_5',['📝 Usage Examples',['../index.html#autotoc_md27',1,'']]],
-  ['user_20interfaces_6',['User Interfaces',['../index.html#autotoc_md8',1,'']]]
+  ['updatesize_4',['updateSize',['../classScapeGoatTree.html#a353a8b23b287c2c22c05436c457ef243',1,'ScapeGoatTree']]],
+  ['usage_5',['Advanced Usage',['../index.html#autotoc_md9',1,'']]],
+  ['usage_20examples_6',['📝 Usage Examples',['../index.html#autotoc_md27',1,'']]],
+  ['user_20interfaces_7',['User Interfaces',['../index.html#autotoc_md8',1,'']]]
 ];
--- search/functions_12.js
+++ search/functions_12.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['undo_0',['undo',['../classScapeGoatTree.html#a693ee93867d1092ca56b98becfddb21d',1,'ScapeGoatTree']]]
+  ['undo_0',['undo',['../classScapeGoatTree.html#a693ee93867d1092ca56b98becfddb21d',1,'ScapeGoatTree']]],
+  ['updatesize_1',['updateSize',['../classScapeGoatTree.html#a353a8b23b287c2c22c05436c457ef243',1,'ScapeGoatTree']]]
 ];
