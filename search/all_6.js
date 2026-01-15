430d90016bb12561b34ce3e279afb71ce2cd5226
OmarAhmedTHE25th <OmarAhmedTHE25th@users.noreply.github.com>
2026-01-15 17:31:56 +0000

    deploy: 8a5b4fd592ed2551fa37ca63f7c82b14753fb7f2

--- search/all_6.js
+++ search/all_6.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['features_0',['features',['../index.html#autotoc_md5',1,'✨ Features'],['../index.html#autotoc_md6',1,'Core Tree Features']]],
-  ['findh_1',['findH',['../classScapeGoatTree.html#ade7915ec9566392558f90918bd798283',1,'ScapeGoatTree']]],
-  ['findsuccessor_2',['findSuccessor',['../classScapeGoatTree.html#a8490db2489c7785ae0b563fef3d91916',1,'ScapeGoatTree']]],
-  ['findtraitor_3',['findTraitor',['../classScapeGoatTree.html#a236fa4811a827ed772c5d4257dcff902',1,'ScapeGoatTree']]],
-  ['front_4',['front',['../classQueue.html#a8b5b32ea305ed921bd06ab62bd53bc1b',1,'Queue']]]
+  ['find_5fnode_1',['find_node',['../classScapeGoatTree.html#a9ab7fd085bf443a3235eb91ef6d4ab11',1,'ScapeGoatTree']]],
+  ['findh_2',['findH',['../classScapeGoatTree.html#ade7915ec9566392558f90918bd798283',1,'ScapeGoatTree']]],
+  ['findsuccessor_3',['findSuccessor',['../classScapeGoatTree.html#a8490db2489c7785ae0b563fef3d91916',1,'ScapeGoatTree']]],
+  ['findtraitor_4',['findTraitor',['../classScapeGoatTree.html#a236fa4811a827ed772c5d4257dcff902',1,'ScapeGoatTree']]],
+  ['front_5',['front',['../classQueue.html#a8b5b32ea305ed921bd06ab62bd53bc1b',1,'Queue']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['findh_0',['findH',['../classScapeGoatTree.html#ade7915ec9566392558f90918bd798283',1,'ScapeGoatTree']]],
-  ['findsuccessor_1',['findSuccessor',['../classScapeGoatTree.html#a8490db2489c7785ae0b563fef3d91916',1,'ScapeGoatTree']]],
-  ['findtraitor_2',['findTraitor',['../classScapeGoatTree.html#a236fa4811a827ed772c5d4257dcff902',1,'ScapeGoatTree']]],
-  ['front_3',['front',['../classQueue.html#a8b5b32ea305ed921bd06ab62bd53bc1b',1,'Queue']]]
+  ['find_5fnode_0',['find_node',['../classScapeGoatTree.html#a9ab7fd085bf443a3235eb91ef6d4ab11',1,'ScapeGoatTree']]],
+  ['findh_1',['findH',['../classScapeGoatTree.html#ade7915ec9566392558f90918bd798283',1,'ScapeGoatTree']]],
+  ['findsuccessor_2',['findSuccessor',['../classScapeGoatTree.html#a8490db2489c7785ae0b563fef3d91916',1,'ScapeGoatTree']]],
+  ['findtraitor_3',['findTraitor',['../classScapeGoatTree.html#a236fa4811a827ed772c5d4257dcff902',1,'ScapeGoatTree']]],
+  ['front_4',['front',['../classQueue.html#a8b5b32ea305ed921bd06ab62bd53bc1b',1,'Queue']]]
 ];
